0d7555ec91dde551a3eefc286e195a667085c4fc
Zain Maruf <theultimategamerz87@gmail.com>
2026-02-04 20:41:41 +0530

    Create next.config.js

---
+++ StreamingApp/next.config.js
@@ -0,0 +1,18 @@
+module.exports = {
+  reactStrictMode: true,
+  images: {
+    domains: ['image.tmdb.org', 'via.placeholder.com'],
+  },
+  async headers() {
+    return [
+      {
+        source: '/api/:path*',
+        headers: [
+          { key: 'Access-Control-Allow-Credentials', value: 'true' },
+          { key: 'Access-Control-Allow-Origin', value: '*' },
+          { key: 'Access-Control-Allow-Methods', value: 'GET,OPTIONS,PATCH,DELETE,POST,PUT' },
+        ],
+      },
+    ];
+  },
+};
